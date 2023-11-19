859df2d47be1313587a14fc6ae3fee4e4f237743
Humphrey <h.nyahoja@alustudent.com>
2023-11-18 16:40:51 -0800

    tried to correct advanced file

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
-#Just as in the previous configuration file task, we’d like you to set up your client SSH configuration file so that you can connect to a server without typing a password
-Host *
-	IdentityFile ~/.ssh/school
-	PassworAuthentication no
+# This script configures the SSH client to use the private key at ~/.ssh/school
+
+class ssh_config {
+
+  file { '/etc/ssh/ssh_config':
+    ensure => file,
+  }
+
+  file_line { 'Declare identity file':
+    path    => '/etc/ssh/ssh_config',
+    line    => 'IdentityFile ~/.ssh/schooli',
+    match   => '^IdentityFile',
+    replace => true,
+  }
+
+  file_line { 'Turn off passwd auth':
+    path    => '/etc/ssh/ssh_config',
+    line    => 'PasswordAuthentication no',
+    match   => '^PasswordAuthentication',
+    replace => true,
+  }
+}
+
+include ssh_config
